c72df634131f8c50b0ef6fb4ae3f720386b61707
Venkatesh Bashetty <parthuvenkatesh@gmail.com>
2017-01-21 13:10:07 -0500

    Angular file got added

---
+++ angular1.js
@@ -0,0 +1,26 @@
+
+var myapp = angular.module('myapp',[]);
+
+myapp.controller('maincontroller',['$scope','$log','$filter','$timeout',function($scope,$log,$filter,$timeout){
+
+   $scope.name='arjuna';
+   $scope.modiefyName = $filter('uppercase')($scope.name);
+   $timeout(function(){
+     $scope.name='Krishna';
+   },1000);
+
+}]);
+
+
+
+/*var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    
+    $scope.handle = '';
+    
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+}]);*/
