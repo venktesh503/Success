103cddef55df99c621fd829c659979ddc7b75e55
Venkatesh Bashetty <parthuvenkatesh@gmail.com>
2017-01-21 15:06:03 -0500

    new Angulr 1.3 Branch file got added

---
+++ angular1.3Branch.js
@@ -0,0 +1,27 @@
+
+var myapp = angular.module('myapp',[]);
+
+myapp.controller('maincontroller',['$scope','$log','$filter','$timeout',function($scope,$log,$filter,$timeout){
+
+   $scope.name='arjuna';
+   $scope.modiefyName = $filter('uppercase')($scope.name);
+   $timeout(function(){
+     $scope.name='Krishna';
+   },1000);
+
+}]);
+
+
+
+/*var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    
+    $scope.handle = '';
+    
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+}]);*/
+New Angular 1.3 Branch got Created for doing Some more Research about it.
